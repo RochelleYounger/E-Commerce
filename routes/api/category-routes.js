27d3d94a1cd83945e291cf6e511dda9451a41607
Rochelle Younger <regnuoy321@gmail.com>
2022-04-21 10:42:18 -0400

    cleaned up code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   Category.findAll({
     include: [
       {
@@ -22,8 +18,6 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   Category.findOne({
     where: {
       id: req.params.id
@@ -49,7 +43,6 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
   Category.create(req.body)
     .then(categoryRes => res.json(categoryRes))
     .catch(err => {
@@ -59,7 +52,6 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
   Category.update(req.body, {
     where: {
         id: req.params.id
@@ -79,7 +71,6 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
   Category.destroy({
     where: {
         id: req.params.id
